5d8baecaf9ae67d519c5b5541fbc54e02260ecb2
Volker Mische <volker.mische@gmail.com>
2023-02-22 13:49:06 +0100

    fix: use correct kernel for blst types

--- poc/blst-cuda/build.rs
+++ poc/blst-cuda/build.rs
@@ -115,7 +115,7 @@ fn main() {
         if let Some(include) = env::var_os("DEP_SPPARK_ROOT") {
             nvcc.include(include);
         }
-        nvcc.file("cuda/pippenger_inf.cu").compile("blst_cuda_msm");
+        nvcc.file("cuda/pippenger.cu").compile("blst_cuda_msm");
 
         println!("cargo:rustc-cfg=feature=\"cuda\"");
         println!("cargo:rerun-if-changed=cuda");
